b877732dd9e4b3fe583cef9cb0a523acd6e3a028
Joy Chebet <160256031+JoyChebet@users.noreply.github.com>
2024-09-12 08:05:02 +0300

    added variables

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Joy Chebet';
+const welcomeMessage = 'contains "Welcome to Flatbook, " Joy Chebet !';
+let excitedWelcomeMessage = 'contains "WELCOME TO FLATBOOK, " JOY CHEBET !';
+let shortGreeting ='contain "Welcome, " J !'
